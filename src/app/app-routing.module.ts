e07383d12942ed087a4a7ec5f1877b0d9c102fd9
sandy27-09 <sudiproydgp100gmail.com>
2023-01-08 01:12:40 +0530

    meow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"sandyhome",component:HomeComponent},
+{path:"sandyproducts",component:ProductsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const sandyrr=[HomeComponent,ProductsComponent];
